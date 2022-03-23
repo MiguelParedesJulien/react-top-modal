fd41a738ece7a3e67ec20cd121f4ba7c1141617f
Miguel Paredes Julien <julien314@hotmail.fr>
2022-03-23 15:07:34 +0100

    Features : create modal with close button + close with escape

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Modal from "components/modal/modal";
+import useModal from "utils/useModal";
+import "App.css";
 
 function App() {
+  const { isShowing: show1, toggle: toggle1 } = useModal();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="salut">
+        <button type="submit" className="buttonDefault" onClick={toggle1}>
+          Modal (with header)
+        </button>
+        <Modal isShowing={show1} hide={toggle1} addCloseEscape={true}>
+          <h1>Hello, je suis un titre </h1>
+          <p>bla bla bla</p>
+        </Modal>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/modal/modal.js
@@ -0,0 +1,52 @@
+import { FaTimes } from "react-icons/fa";
+import React, { useEffect } from "react";
+import ReactDom from "react-dom";
+import "components/modal/modal.css";
+
+function Modal({ isShowing, hide, children, addCloseEscape }) {
+  useEffect(() => {
+    return window.addEventListener("keyup", (e) => {
+      if (addCloseEscape) {
+        closeModalEvent(e);
+      }
+    });
+  });
+
+  const closeModal = () => {
+    if (isShowing) {
+      hide();
+    }
+  };
+
+  const closeModalEvent = (e) => {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  };
+  return isShowing
+    ? ReactDom.createPortal(
+        <>
+          <div className="modalOverlay">
+            <div className="modalWrapper">
+              <aside className="modal">
+                <header className="modalHeader">
+                  <button aria-label="Close" className="modalCloseButton" data-dismiss="modal" onClick={hide} type="button">
+                    <FaTimes />
+                  </button>
+                </header>
+                <section className="modalSection">{children}</section>
+                <footer className="modalFooter">
+                  <button className="modalButton" onClick={hide}>
+                    Close Modal
+                  </button>
+                </footer>
+              </aside>
+            </div>
+          </div>
+        </>,
+        document.getElementById("portal")
+      )
+    : "";
+}
+
+export default Modal;
